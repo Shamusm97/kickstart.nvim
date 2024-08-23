8900297361780dd7f3ad1372fa337b944747e607
Shamusm97 <shamus.mckeown@icloud.com>
2024-08-24 01:02:58 +1200

    rework init. add custom plugins

---
+++ lua/custom/plugins/autosession.lua
@@ -0,0 +1,28 @@
+return {
+  'rmagatti/auto-session',
+  lazy = false,
+  dependencies = {
+    'nvim-telescope/telescope.nvim',
+  },
+  keys = {
+    -- Will use Telescope if installed or a vim.ui.select picker otherwise
+    { '<C-s>', '<cmd>SessionSearch<CR>', desc = 'Session search' },
+  },
+  config = function()
+    require('auto-session').setup({
+      -- ⚠️ This will only work if Telescope.nvim is installed
+      -- The following are already the default values, no need to provide them if these are already the settings you want.
+      session_lens = {
+        -- If load_on_setup is false, make sure you use `:SessionSearch` to open the picker as it will initialize everything first
+        load_on_setup = true,
+        theme_conf = { border = true },
+        previewer = false,
+        mappings = {
+          -- Mode can be a string or a table, e.g. {"i", "n"} for both insert and normal mode
+          delete_session = { "i", "<C-D>" },
+          alternate_session = { "i", "<C-S>" },
+        },
+      },
+    })
+  end,
+}
